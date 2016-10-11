a759585c5f804e9be2b95b26aa1f3eef0062597e
Alex Palmer <xanderx@xanderx.com>
2016-10-11 20:17:40 +0100

    Allow restores before the teamspeak container has been created

--- ts3-restore.sh
+++ ts3-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Stop server before restoring to prevent data inconsistency
-docker stop teamspeak
+docker inspect --type container teamspeak >/dev/null 2>&1 && docker stop teamspeak
 docker run -it --rm --volume teamspeak:/data --volume "$(pwd):/backup" teamspeak bash -c "find /data -mindepth 1 -depth -delete && tar -xvf \"/backup/${1}\" -C /"
-docker start teamspeak
+docker inspect --type container teamspeak >/dev/null 2>&1 && docker start teamspeak
 
