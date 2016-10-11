c2331d5eab1c6b9be5beeeb1e573473c45b08270
Alex Palmer <xanderx@xanderx.com>
2016-10-11 19:26:16 +0100

    Store persistent data in a "teamspeak" named volume

--- ts3-restore.sh
+++ ts3-restore.sh
@@ -2,6 +2,6 @@
 
 # Stop server before restoring to prevent data inconsistency
 docker stop teamspeak
-docker run -it --rm --volumes-from teamspeak-data --volume "$(pwd):/backup" teamspeak bash -c "find /data -mindepth 1 -depth -delete && tar -xvf \"/backup/${1}\" -C /"
+docker run -it --rm --volume teamspeak:/data --volume "$(pwd):/backup" teamspeak bash -c "find /data -mindepth 1 -depth -delete && tar -xvf \"/backup/${1}\" -C /"
 docker start teamspeak
 
--- ts3-run.sh
+++ ts3-run.sh
@@ -2,9 +2,5 @@
 # Remove any running instance
 docker rm -f teamspeak > /dev/null 2>&1
 
-# Data-only volume -- backup just in case, only create if it doesn't exist so as to not lose data
-./ts3-backup.sh
-docker inspect teamspeak-data > /dev/null 2>&1 || docker run --name teamspeak-data teamspeak /bin/echo "Data-only container for TeamSpeak"
-
-# Server
-docker run --name teamspeak --detach --restart unless-stopped --publish 9987:9987/udp --publish 10011:10011 --publish 30033:30033 --volumes-from teamspeak-data --memory 100m teamspeak
+# Run TeamSpeak server
+docker run --name teamspeak --detach --restart unless-stopped --publish 9987:9987/udp --publish 10011:10011 --publish 30033:30033 --volume teamspeak:/data --memory 100m teamspeak
