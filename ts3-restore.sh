b40593f1e6d7c7b6d4f5c7534711456a3b5a9dfd
Alex Palmer <xanderx@xanderx.com>
2017-01-21 15:41:02 +0000

    When resotring check archive is specified and exists

--- ts3-restore.sh
+++ ts3-restore.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Archive must be specified and readable
+[ $# -eq 0 -o ! -r "$1" ] && {
+  echo "Usage: $0 <archive>"
+  exit 1
+}
+
 # Stop server before restoring to prevent data inconsistency
 docker inspect --type container teamspeak >/dev/null 2>&1 && docker stop teamspeak
 docker run -it --rm --volume teamspeak:/data --volume "$(pwd):/backup" teamspeak bash -c "find /data -mindepth 1 -depth -delete && tar -xvf \"/backup/${1}\" -C /"
 docker inspect --type container teamspeak >/dev/null 2>&1 && docker start teamspeak
-
